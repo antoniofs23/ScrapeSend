a303409716409ee74a235386612d740bde5cd724
Antonio Fernandez <af3036@nyu.edu>
2023-10-13 22:45:45 -0400

    Update ScrapeRates.py

--- ScrapeRates.py
+++ ScrapeRates.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+#
 def ScrapeRates(target_url= "https://www.mortgagenewsdaily.com/mortgage-rates"):
     tempR = []
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
